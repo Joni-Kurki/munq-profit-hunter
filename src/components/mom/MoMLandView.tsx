d35e1e0620e2167198dc642e4cea6ea7c23a319f
Joni-Kurki <joni.kurki@gmail.com>
2022-08-02 16:27:31 +0300

    - filtering by price - some cosmetic changes

--- src/components/mom/MoMLandView.tsx
+++ src/components/mom/MoMLandView.tsx
@@ -2,9 +2,9 @@ import { Grid, Typography } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react";
 import { API_ENDPOINT_URL } from "../../common/constants"
-import { MoMFilterTypeEnum } from "../../common/enum";
+import { MoMLandRarityFilterEnum } from "../../common/enum";
 import { getRarityNumberFromEnum, sortLandPlotsByPrice } from "../../common/helper";
-import { IGetLandPlotsResponse, ILandPlot, IMoMFilter } from "../../interfaces/IMoMInteface";
+import { IGetLandPlotsResponse, ILandPlot } from "../../interfaces/IMoMInteface";
 import { IMoMLandPlotRowProps, MoMLandPlotRow } from "./MoMLandPlotRow";
 import { IMoMStickyFiltersHeaderProps, MoMStickyFiltersHeader } from "./MoMStickyFiltersHeader";
 
@@ -12,6 +12,15 @@ export const MoMLandView = () => {
 
 	const [allLandPlotData, setAllLandPlotData] = useState<ILandPlot[] | null>(null);
 	const [filteredLandPlotData, setFilteredLandPlotData] = useState<ILandPlot[] | null>(null);
+	const [landsRarities, setLandsRarities] = useState<number[]>([
+		MoMLandRarityFilterEnum.Common,
+		MoMLandRarityFilterEnum.Uncommon,
+		MoMLandRarityFilterEnum.Rare,
+		MoMLandRarityFilterEnum.Epic,
+		MoMLandRarityFilterEnum.Legendary
+	]);
+	const [landsMinPrice, setLandsMinPrice] = useState<number>(-Infinity);
+	const [landsMaxPrice, setLandsMaxPrice] = useState<number>(Infinity);
 
 	const fetchLandPlotData = () => {
 		axios.get<IGetLandPlotsResponse>(`${API_ENDPOINT_URL}/api/mom/getLandPlots`)
@@ -26,34 +35,50 @@ export const MoMLandView = () => {
 		setFilteredLandPlotData(allLandPlotData);
 	}, [allLandPlotData])
 
-	useEffect(() => {
-		console.log(filteredLandPlotData)
-	}, [filteredLandPlotData])
-
-	const onLandRarityFilterChange = (filters: number[]) => {
+	const filterChange = () => {
 		if(!allLandPlotData) return;
 
 		let allLandPlots:ILandPlot [] = [];
 
-		for(let rarity of filters){
+		for(let rarity of landsRarities){
 			allLandPlots = allLandPlots.concat(allLandPlotData.filter(p => getRarityNumberFromEnum(p.rarity) === rarity))
 		}	
 		
 		allLandPlots = allLandPlots.concat(allLandPlotData.filter(p => p.rarity === null));
 
+		allLandPlots = allLandPlots.filter(a => a.price.amount >= landsMinPrice && a.price.amount <= landsMaxPrice);
+
 		setFilteredLandPlotData(sortLandPlotsByPrice([...allLandPlots]));
 	}
 
+	const onLandRarityFilterChange = (filters: number[]) => {
+		setLandsRarities(filters);
+	}
+
+	const onLandPriceChange = (value: number, isMin: boolean) => {
+		if(isMin)
+			setLandsMinPrice(value === 0 ? -Infinity : value);
+		else 
+			setLandsMaxPrice(value === 0 ? Infinity : value);
+	}
+
+	useEffect(() => {
+		filterChange();
+	}, [landsMinPrice, landsMaxPrice, landsRarities])
+
 	const moMStickyFiltersHeaderProps: IMoMStickyFiltersHeaderProps = {
 		fetchLandPlotData: fetchLandPlotData,
-		onLandRarityFilterChange: (filters: number[]) => { onLandRarityFilterChange(filters) }
+		onLandRarityFilterChange: (filters: number[]) => { onLandRarityFilterChange(filters) },
+		onLandPriceChange: (value: number, isMin: boolean) => { onLandPriceChange(value, isMin) },
+		allLandsCount: allLandPlotData?.length ?? null,
+		filteredLandsCount: filteredLandPlotData?.length ?? null
 	}
 
 	return(
 		<Grid container>
 			<Grid container>
 				<Grid container>
-					<Typography variant="h2">MoM land plot scanner</Typography>
+					<Typography variant="h4">MoM land plot scanner by munqqqcrypto</Typography>
 				</Grid>
 				
 				<MoMStickyFiltersHeader {...moMStickyFiltersHeaderProps} />
--- src/components/mom/MoMStickyFiltersHeader.tsx
+++ src/components/mom/MoMStickyFiltersHeader.tsx
@@ -1,12 +1,14 @@
-import { Button, Checkbox, Grid, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent } from "@mui/material";
+import { Button, Checkbox, FilledInput, FormHelperText, Grid, InputAdornment, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { MoMFilterTypeEnum, MoMLandRarityFilterEnum } from "../../common/enum";
-import { getLandChipColorByRarity, getMoMDropdownMenuItems } from "../../common/helper";
-import { IMoMFilter, IMoMLandPlotFilter } from "../../interfaces/IMoMInteface";
+import { MoMLandRarityFilterEnum } from "../../common/enum";
+import { getLandChipColorByRarity } from "../../common/helper";
 
 export interface IMoMStickyFiltersHeaderProps {
 	fetchLandPlotData: () => void;
 	onLandRarityFilterChange: (rarities: number[]) => void;
+	onLandPriceChange: (value: number, isMin: boolean) => void;
+	allLandsCount: number | null;
+	filteredLandsCount: number | null;
 }
 
 export const MoMStickyFiltersHeader = (props: IMoMStickyFiltersHeaderProps) => {
@@ -17,6 +19,8 @@ export const MoMStickyFiltersHeader = (props: IMoMStickyFiltersHeaderProps) => {
 		MoMLandRarityFilterEnum.Epic,
 		MoMLandRarityFilterEnum.Legendary
 	]);
+	const [minPrice, setMinPrice] = useState<number>(0);
+	const [maxPrice, setMaxPrice] = useState<number>(0);
 
 	const handleLandRarityFilterChange = (value: number, checked: boolean) => {
 		let selectedValues = [...selectedLandRarities];
@@ -31,13 +35,26 @@ export const MoMStickyFiltersHeader = (props: IMoMStickyFiltersHeaderProps) => {
 		setSelectedLandRarities([...selectedValues].sort((a,b) => a - b));
 	}
 
+	const handlePriceChange = (value: string, isMin: boolean) => {
+		const tempV = Number(value);
+
+		if(!isNaN(tempV) && isMin)
+			setMinPrice(tempV);
+		else if(!isNaN(tempV) && !isMin)
+			setMaxPrice(tempV);
+		else 
+			return;
+
+		props.onLandPriceChange(tempV, isMin);
+	}
+
 	const isLandRarityChecked = (value: number) => {
 		return selectedLandRarities.findIndex(v => v === value) > -1;
 	}
 
 	useEffect(() => {
 		props.onLandRarityFilterChange(selectedLandRarities);
-	}, [selectedLandRarities])
+	}, [props, selectedLandRarities])
 
 	const landRaritiesCheckBoxes = [
 		{ label: "Common", value: MoMLandRarityFilterEnum.Common, color: getLandChipColorByRarity("Common")  },
@@ -50,15 +67,20 @@ export const MoMStickyFiltersHeader = (props: IMoMStickyFiltersHeaderProps) => {
 	return (
 		<Grid container style={{ borderBottom: "solid 1px white" }}>
 			<Grid container >
-				<Button	variant="outlined" onClick={props.fetchLandPlotData}>Load the data</Button>
+				<Button	variant="outlined" onClick={props.fetchLandPlotData}>Load land plots data</Button>
 			</Grid>
 
-			<Grid container>
-				<Grid item xs={12}>
+			<Grid container style={{paddingBottom: "0.5rem"}}>
+				<Grid item xs={12} style={{borderTop: "2px solid white", borderBottom: "2px solid white"}}>
 					<Grid container style={{ alignItems: "center" }}>
-						<Grid item xs={1}>
-							Land rarities
+						<Grid item xs={1} >
+							<Typography variant="h5">Lands</Typography>
 						</Grid>
+
+						<Grid item xs={1} style={{textAlign: "left" }}>
+							<Typography variant="h6">rarities</Typography>
+						</Grid>
+
 						<Grid item xs={3}>
 							{ landRaritiesCheckBoxes.map(box => 
 								<Checkbox
@@ -70,11 +92,41 @@ export const MoMStickyFiltersHeader = (props: IMoMStickyFiltersHeaderProps) => {
 								/>
 							) }
 						</Grid>
-						<Grid item xs={1}>
-							Price
-						</Grid>
+
 						<Grid item xs={4}>
-							min 150, max 300
+							<Grid container>
+								<Grid item xs={4}>
+									<FilledInput
+										endAdornment={<InputAdornment position="end">WAX</InputAdornment>}
+										id="mom-filter-min-price-input"
+										inputProps={{
+											"aria-label": "min price"
+										}}
+										onChange={(e) => handlePriceChange(e.target.value, true)}
+										size="small"
+										style={{ backgroundColor: "#121212"}}
+										value={minPrice === 0 ? "" : minPrice}
+									/>
+									<FormHelperText id="mom-filter-min-price-input-text">min price</FormHelperText>
+								</Grid>
+								<Grid item xs={4} style={{paddingLeft: "0.5rem"}}>
+									<FilledInput
+										endAdornment={<InputAdornment position="end">WAX</InputAdornment>}
+										id="mom-filter-max-price-input"
+										inputProps={{
+											"aria-label": "max price"
+										}}
+										onChange={(e) => handlePriceChange(e.target.value, false)}
+										size="small"
+										style={{ backgroundColor: "#121212"}}
+										value={maxPrice === 0 ? "" : maxPrice}
+									/>
+									<FormHelperText id="mom-filter-max-price-input-text">max price</FormHelperText>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={3}>
+							{ props.allLandsCount && (`${props.filteredLandsCount ?? 0} of ${props.allLandsCount} lands shown.`) }
 						</Grid>
 					</Grid>
 				</Grid>
